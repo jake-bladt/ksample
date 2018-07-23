4b478e5f907dfd71a585099fb8be74c0276f47b7
Jake Bladt <jake@jakebladt.com>
2018-07-23 00:18:00 -0400

    +actions

--- api/assignments/actions.js
+++ api/assignments/actions.js
@@ -1,7 +1,20 @@
-module.exports = (repo) => ({
-  getById: (id) => undefined,
-  getByTag: (id) => undefined,
-  create: (assignment) => {
-      const 
+const connect = require('./schema');
+
+module.exports = (config) => ({
+  createAssignment: (assignment) => {
+    return connect(config)
+      .then((db) => {
+        const newAssignment = new db.Assignment(assignment);
+        return newAssignment.save;
+      });
+  },
+  getAssignment: (id) => {
+    return connect(config)
+      .then((db) => db.Assignment.findById(id));
+  },
+  getAssignments: (tag) => {
+    return connect((config)
+      .then((db) => db.Assignment.find({ tag }))
+    )
   }
 });
